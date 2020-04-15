4ff94fb93712f52bd477cd6294980173320b447a
Mithra Desinguraj <mithradesinguraj@Mithras-MacBook-Air.local>
2020-04-14 17:16:49 -0700

    Updates

---
+++ precache-manifest.19910f9157034099d63fe42aa8617c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031f7786d7cd2c3e003219d06e40f21c",
+    "url": "/RedditClient/index.html"
+  },
+  {
+    "revision": "13d6c9e43e632b9b8833",
+    "url": "/RedditClient/static/css/main.801930a0.chunk.css"
+  },
+  {
+    "revision": "b56cd16a3dc111332875",
+    "url": "/RedditClient/static/js/2.ea7d83d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RedditClient/static/js/2.ea7d83d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d6c9e43e632b9b8833",
+    "url": "/RedditClient/static/js/main.f0a75913.chunk.js"
+  },
+  {
+    "revision": "26198ed5f3eaf61da0cd",
+    "url": "/RedditClient/static/js/runtime-main.ac5d9e04.js"
+  }
+]);
