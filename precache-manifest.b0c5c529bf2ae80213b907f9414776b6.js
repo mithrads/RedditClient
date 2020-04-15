6c20bd7e8b9dc53283f9742f45fe96c4eb4cb7f8
Mithra Desinguraj <mithradesinguraj@Mithras-MacBook-Air.local>
2020-04-14 18:25:37 -0700

    Updates

--- precache-manifest.b0c5c529bf2ae80213b907f9414776b6.js
+++ precache-manifest.b0c5c529bf2ae80213b907f9414776b6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f4a664da75be130243592c934f631d2",
+    "revision": "10f8d7fdbe5680cd01ce1a06749a3f49",
     "url": "/RedditClient/index.html"
   },
   {
-    "revision": "94ed9f7ae4ca6aa537e5",
+    "revision": "d5ffec28d9e8c1f7be54",
     "url": "/RedditClient/static/css/main.c0a9515b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/RedditClient/static/js/2.ea7d83d3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "94ed9f7ae4ca6aa537e5",
-    "url": "/RedditClient/static/js/main.1861cb93.chunk.js"
+    "revision": "d5ffec28d9e8c1f7be54",
+    "url": "/RedditClient/static/js/main.37fa328e.chunk.js"
   },
   {
     "revision": "26198ed5f3eaf61da0cd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RedditClient/precache-manifest.48a4f2ab3ca1f5960b771e24b3b8527d.js"
+  "/RedditClient/precache-manifest.b0c5c529bf2ae80213b907f9414776b6.js"
 );
 
 self.addEventListener('message', (event) => {
